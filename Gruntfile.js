b1ddac419da4bb7ad7e4fe8f539f93ce685e26b7
Lucas <metallucas.ds@gmail.com>
2013-04-28 12:46:29 -0300

    index

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
 				files: [
 					'<%= meta.srcPath %>*.scss'
 				],
-				tasks: ['sass', 'livereload']
+				tasks: ['sass']
 			}
 		}
 	});
@@ -71,5 +71,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['sass', 'livereload-start', 'connect', 'regarde']);
+	grunt.registerTask('default', ['watch']);
 };
