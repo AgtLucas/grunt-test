c5c49186e321094108691574f73691c982678a56
Lucas <metallucas.ds@gmail.com>
2013-04-30 23:35:25 -0300

    Adding livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,10 @@
 var path = require('path');
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 
+var folderMount = function folderMount(connect, point) {
+	return connect.static(path.resolve(point));
+};
+
 module.exports = function(grunt) {
 
 	//	Project configuration
@@ -26,6 +30,21 @@ module.exports = function(grunt) {
 				},
 				options: {
 					style: 'compressed'
+				}
+			}
+		},
+
+		livereload: {
+			port: 35729
+		},
+
+		connect: {
+			livereload: {
+				options: {
+					port: 9001,
+					middleware: function(connect, options) {
+						return [lrSnippet, folderMount(connect, options.base)]
+					}
 				}
 			}
 		},
